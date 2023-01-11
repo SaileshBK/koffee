e520adb81abb5a4c8d9a7227532e1d7cd5dd22b9
SaileshBK <sailesh.bk1@icloud.com>
2023-01-11 01:09:18 -0600

    main body initial setup finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyCoffeeComponent } from './my-coffee/my-coffee.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyCoffeeComponent,
-    FooterComponent
+    FooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
