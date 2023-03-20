0e15e47c629bc9d2c8b4c2b50691220986c5ea62
SaileshBK <sailesh.bk1@icloud.com>
2023-03-20 01:28:20 -0500

    chore(master): added more nav items

--- src/app/nav-bar/nav-bar-data.ts
+++ src/app/nav-bar/nav-bar-data.ts
@@ -3,5 +3,26 @@ export const sideNavBarData = [
     routeLink: 'home',
     icon: 'bx bxs-home bx-tada',
     label: 'Home'
+  },
+  {
+    routeLink: 'About',
+    icon: 'bx bxs-info-circle bx-tada',
+    label: 'About'
+  },
+  {
+    routeLink: 'Products',
+    icon: 'bx bxs-coffee-bean bx-tada',
+    label: 'Products'
+  },
+  {
+    routeLink: 'gallery',
+    icon: 'bx bxs-image bx-tada',
+    label: 'Gallery'
+  },
+  {
+    routeLink: 'contact',
+    icon: 'bx bxs-contact bx-tada',
+    label: 'Contact'
   }
+
 ];
