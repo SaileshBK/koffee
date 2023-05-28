faf1c03cb3678a9e8703354c36a778e25e3debe1
SaileshBK <sailesh.bk1@icloud.com>
2023-05-27 23:31:45 -0500

    chore(master): cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HotKoffeeComponent } from './hot-koffee/hot-koffee.component';
 import { FooterComponent } from './footer/footer.component';
