e09a25072832f2752a1f86209c4f55eda0f17e79
SaileshBK <sailesh.bk1@icloud.com>
2024-02-18 00:41:08 -0600

    chore(master): Refactored some stuffs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HotKoffeeComponent } from './hot-koffee/hot-koffee.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainContentComponent } from './main-content/main-content.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { GalleryComponent } from './nav-bar/gallery/gallery.component';
 import { RouterModule } from '@angular/router';
@@ -19,7 +17,6 @@ import { HomeComponent } from './nav-bar/home/home.component';
     AppComponent,
     HotKoffeeComponent,
     FooterComponent,
-    MainContentComponent,
     NavBarComponent,
     GalleryComponent,
     AboutComponent,
@@ -31,7 +28,6 @@ import { HomeComponent } from './nav-bar/home/home.component';
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: MainContentComponent },
       { path: 'nav-bar/home', component: HomeComponent },
       { path: 'nav-bar/gallery', component: GalleryComponent },
       { path: 'nav-bar/about', component: AboutComponent },
--- src/app/main-content/main-content.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-main-content',
-  templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.scss']
-})
-export class MainContentComponent {
-}
